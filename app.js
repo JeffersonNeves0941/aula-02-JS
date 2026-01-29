91db51d42caaa1e88461232c1cfed2ff78a8df30
JEFFERSON VICTOR NEVES DA SILVA <25204074@cfp127aluno.local>
2026-01-29 17:05:00 -0300

    Validando entrada de dados

--- app.js
+++ app.js
@@ -15,11 +15,76 @@ Versão: 1.0
            Dica: CAso voe precise utilizar o var, recomenda-se 
            que seja utilizado apenas em escopo global  
 
-    let => 
+    let => Permite criar um espaço em memoria do tipo variável 
+        Essa forma de criação é realizada somente no escopo local ,
+        ou seja dentro de bloco de programação {}.
+        Esse tipo de variável deixa de existir ao término do bloco 
 
-    const =>
-
+    const => Permite criar um espaço em memória do tipo constante, 
+             ou seja esse conteudo não podera sofrer mudanças  durante 
+             o projeto 
+             Dica: Se possivel voce pode criar esse const escrita em MAIUSCULO para 
+             facilitar a sua utilização. Pode ser criada de forma local ou forma global
 */
+/*
+Operadores de comparação 
+
+        == => Permite a comparação de dois conteudos
+        != => Permite comparar a diferença de dois conteudo
+        < => permite validar valor menor 
+        > => Permite validar valor maior 
+        <= -> Permite validar valor menor ou igual
+        >= -> Permite validar valor maior ou igual 
+        === => permite comparar a igualdade dos conteudos e a
+        igualdade da tipagem de dados 
+        !== Permite comparar a diferenças do conteudo e a
+        igualdade de tipagem de daods
+        ==! Permite comparar a igualdade de conteudo e a 
+        diferença de tipagem de dados 
+
+        Tipos de operador logicos 
+            E =>  AND => &&
+            E => OU => ||
+            E => NOT = !
+        */
 
 
-    console.log("Digite o nome do aluno: ")
+
+//Import da biblioteca de entrada de dados 
+const readline = require("readline")
+//Criação do objeto para captar as entradas de dados 
+const entradaDeDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+//Entrada de dados do nome
+entradaDeDados.question("Digite o nome do aluno: ", function (nome) {
+    //Receve o nome do aluno
+    let nomeAluno = nome
+
+    entradaDeDados.question("Digite a nota1: ", function (valor1) {
+        let nota1 = valor1
+
+        entradaDeDados.question("Digite a nota2: ", function (valor2) {
+            let nota2 = valor2
+
+            entradaDeDados.question("Digite a nota3: ", function (valor3) {
+                let nota3 = valor3
+
+                entradaDeDados.question("Digite a nota4: ", function (valor4) {
+                    let nota4 = valor4
+                    //validação de entrada
+                    if (nomeAluno == "" || nota1 == "" || nota2 == "" || nota3 == "" || nota4 == "") {
+                        console.log("ERROR")
+                    } else if (nota1 < 0 || nota2 < 0 || nota3 < 0 || nota4 < 0) {
+                        console.log("ERROR: Somente são possivel valores entre 0 até 100")
+                    } else if (nota1 > 100 || nota2 > 100 || nota3 > 100 || nota4 > 100) {
+                        console.log("ERROR: Somente são possivel valores entre 0 até 100")
+                    } else if(isNaN(nota1)|| isNaN(nota2)|| isNaN(nota3)|| isNaN(nota4)  ) {
+                        console.log("ERROR: Somente numeros sao permitidos na entrada das notasJ" )
+                    }
+                })
+            })
+        })
+    })
+})
