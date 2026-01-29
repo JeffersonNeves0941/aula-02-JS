b55f2a318920064352d27aa2a6af3d42a231a627
JEFFERSON VICTOR NEVES DA SILVA <25204074@cfp127aluno.local>
2026-01-29 13:58:44 -0300

    Anotações sobre variaveis

---
+++ app.js
@@ -0,0 +1,25 @@
+/***********************************************************
+Obejetivo: Projeto para realizar o calculo de media escolares
+Autor: Jefferson 
+Data: 29/01/2026
+Versão: 1.0
+************************************************************/
+
+/*
+    Tipos de variáveis 
+
+    var => Permite criar um espaço na memoria do tipo variável
+           Essa forma de criação hoje é considerado mais antiga,
+           é provavel que seja encontrada apenas em projetos
+           mais antigos.
+           Dica: CAso voe precise utilizar o var, recomenda-se 
+           que seja utilizado apenas em escopo global  
+
+    let => 
+
+    const =>
+
+*/
+
+
+    console.log("Digite o nome do aluno: ")
